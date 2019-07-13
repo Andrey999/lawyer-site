d16424d8db952272c4b91a5ffa831536fa729863
Andrey999 <gromovandrey1985@gmail.com>
2019-07-13 12:30:24 +0300

    add section new, footer, google maps

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,11 @@ $(function() {
       prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
    });
 
+   $('.header__btn-menu').on('click', function() {
+      $('.menu ul').slideToggle();
+   });
+
 
    // form - styler
     $('select').styler();
-
 });
